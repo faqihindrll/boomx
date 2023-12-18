cb9742ad2bd6d3b5d3520fa51a882e61cace0bb3
Faqih Indra Lesmana <222112039@stis.ac.id>
2023-12-19 00:42:01 +0700

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, return_template
+from flask import Flask, jsonify, request, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return return_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
