daa431cbc81e3628165a64f8f071313e840e6586
Faqih Indra Lesmana <222112039@stis.ac.id>
2023-12-19 01:01:10 +0700

    gf

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/anggota')
+def anggota():
+    return render_template('anggota.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
